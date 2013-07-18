662e233848d13fd0084fe8ed893192380728e400
Andres Ludwig <ludwig.andres@gmail.com>
2013-07-18 01:26:07 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intento2::Application.config.secret_token = '03d300e33f5456a46243317b65578a666d3d7d24772bdf548e0dfb902d34f71a19b53d546549cfd286f506564d772426d3a721423b1c0df62f270f514db7ea00'
